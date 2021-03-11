6a98f1a0186449b5637b416914ac295d4f872f39
heidysnj <heidysnunez15@gmail.com>
2021-03-11 01:49:39 +0000

    review

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,21 @@
-const getState = ({ getStore, setStore }) => {
+const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+
+			getContacts: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/heidysAgenda")
+					.then(res => res.json())
+					.then(response => {
+						console.log(contacts);
+						setStore({ contacts: response });
+					});
+			}
 		}
 	};
 };
