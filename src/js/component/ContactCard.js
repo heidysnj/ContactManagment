7fd767cc18072fca075db166d1d10519cd14c4ae
heidysnj <heidysnunez15@gmail.com>
2021-03-11 02:04:22 +0000

    get ready

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -14,45 +14,47 @@ export const ContactCard = ({ item }) => {
 		<li className="list-group-item">
 			{store.contacts &&
 				store.contacts.map((item, index) => {
-					<div key={index} className="row w-100">
-						<div className="col-12 col-sm-6 col-md-3 px-0">
-							<img
-								src={MikePhoto}
-								alt="Mike Anamendolla"
-								className="rounded-circle mx-auto d-block img-fluid"
-							/>
-						</div>
-						<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
-							<div className=" float-right">
-								<button className="btn">
-									<i className="fas fa-pencil-alt mr-3" />
-								</button>
-								<button className="btn" onClick={() => props.onDelete()}>
-									<i className="fas fa-trash-alt" />
-								</button>
+					return (
+						<div key={index} className="row w-100">
+							<div className="col-12 col-sm-6 col-md-3 px-0">
+								<img
+									src={MikePhoto}
+									alt="Mike Anamendolla"
+									className="rounded-circle mx-auto d-block img-fluid"
+								/>
 							</div>
-							<label className="name lead">{item.full_name}</label>
-							<br />
-							<i className="fas fa-map-marker-alt text-muted mr-3" />
-							<span className="text-muted">{item.address}</span>
-							<br />
-							<span
-								className="fa fa-phone fa-fw text-muted mr-3"
-								data-toggle="tooltip"
-								title=""
-								data-original-title="(870) 288-4149"
-							/>
-							<span className="text-muted small">{item.phone}</span>
-							<br />
-							<span
-								className="fa fa-envelope fa-fw text-muted mr-3"
-								data-toggle="tooltip"
-								data-original-title=""
-								title=""
-							/>
-							<span className="text-muted small text-truncate">{item.email}</span>
+							<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
+								<div className=" float-right">
+									<button className="btn">
+										<i className="fas fa-pencil-alt mr-3" />
+									</button>
+									<button className="btn" onClick={() => props.onDelete()}>
+										<i className="fas fa-trash-alt" />
+									</button>
+								</div>
+								<label className="name lead">{item.full_name}</label>
+								<br />
+								<i className="fas fa-map-marker-alt text-muted mr-3" />
+								<span className="text-muted">{item.address}</span>
+								<br />
+								<span
+									className="fa fa-phone fa-fw text-muted mr-3"
+									data-toggle="tooltip"
+									title=""
+									data-original-title="(870) 288-4149"
+								/>
+								<span className="text-muted small">{item.phone}</span>
+								<br />
+								<span
+									className="fa fa-envelope fa-fw text-muted mr-3"
+									data-toggle="tooltip"
+									data-original-title=""
+									title=""
+								/>
+								<span className="text-muted small text-truncate">{item.email}</span>
+							</div>
 						</div>
-					</div>;
+					);
 				})}
 		</li>
 	);
