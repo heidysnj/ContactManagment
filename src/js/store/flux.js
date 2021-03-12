e61699024e59c893069a1164e47d26f89ee6b4ff
heidysnj <heidysnunez15@gmail.com>
2021-03-12 03:33:48 +0000

    add Contact

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -15,7 +15,31 @@ const getState = ({ getStore, getActions, setStore }) => {
 						console.log(contacts);
 						setStore({ contacts: response });
 					});
-			}
+            },
+            addContact(name,phone,email,address){
+                fetch("https://assets.breatheco.de/apis/fake/contact/", {
+                        method: "post",
+                        headers: {"Content-type": "application/json"},
+                        body: JSON.stringify({
+                            full_name: name,
+                            phone: phone,
+                            address: address,
+                            email:email,
+                            agenda_slug:"heidysAgenda"
+                        }) 
+                }).then(()=>{
+                    fetch("https://assets.breatheco.de/apis/fake/contact/agenda/heidysAgenda")
+                    .then(response => response.json())
+                    .then(result => {
+                        console.log("result", result),
+                        setStore({
+                            contacts: result 
+                        });
+                    })
+                    .catch(e => console.error(e));
+                })
+                
+            }
 		}
 	};
 };
